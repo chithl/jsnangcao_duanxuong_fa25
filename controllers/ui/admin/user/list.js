787e6c8a92cde9c3be46fb1fff295dbfb2086c76
ngoc-2912 <ntnn29122006@gmail.com>
2025-12-19 14:19:10 +0700

    #14 NgocNTNPC09921 Hiển thị địa chỉ theo người dùng

--- controllers/ui/admin/user/list.js
+++ controllers/ui/admin/user/list.js
@@ -60,6 +60,11 @@ async function loadData() {
                                 class="inline-flex items-center justify-center gap-1 rounded-full bg-blue-light-50 px-2.5 py-0.5 text-sm font-medium text-blue-light-500 dark:bg-blue-light-500/15 dark:text-blue-light-500">
                                 Sửa
                               </a>
+
+                              <a href="user-address.html?id=${element.id}"
+                                class="inline-flex items-center justify-center gap-1 rounded-full bg-blue-light-50 px-2.5 py-0.5 text-sm font-medium text-blue-light-500 dark:bg-blue-light-500/15 dark:text-blue-light-500">
+                                Sửa
+                              </a>
                             </div>
                           </td>
                           
